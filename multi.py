bb8937bfc49513c1f20ab9359f0396f692ef3a24
jihan-11 <127156455+jihan-11@users.noreply.github.com>
2023-05-10 19:25:01 +1000

    Copied multiplayer code into google docs and put it back into vs code and now multiplayer works

--- multi.py
+++ multi.py
@@ -1,64 +1,71 @@
 import random
 
-briefcases = list(range(1,27))
-winning_amounts = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 250000, 500000, 750000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000]
+# Set up the initial game variables
+briefcases = list(range(1, 27))
+prize_amounts = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000]
 chosen_cases = []
 players = []
-multiplayer = ()
 
+# Define the functions for the game
 def print_board():
     print("Here are the remaining briefcases:")
     for briefcase in briefcases:
         if briefcase not in chosen_cases:
-            print(briefcase, end='')
+            print(briefcase, end=' ')
     print("\n")
-
+    
 def offer():
-    total_winning_amount = sum(winning_amounts)
-    average_winning_amount= total_winning_amount / len(briefcases)
-    offer_amount = round(average_winning_amount * 0.5, 2 )
+    total_prize_amount = sum(prize_amounts)
+    average_prize_amount = total_prize_amount / len(briefcases)
+    offer_amount = round(average_prize_amount * 0.5, 2)
     return offer_amount
-
+    
 def play_game():
+    # Ask for the number of players
     num_players = int(input("How many players? "))
-
+    
+    # Get the player names
     for i in range(num_players):
         player_name = input(f"What is player {i+1}'s name? ")
         players.append(player_name)
-
-for player_name in players:
-    chosen_case = random.choice(briefcases)
-    chosen_cases.append(chosen_case)
-    briefcases.remove(chosen_case)
-    print(f"{player_name}, your chosen briefcase is: {chosen_case}")
-
-num_rounds = 6
-for round_num in range(num_rounds):
-    print(f"\n\nRound {round_num+1}")
-    print_board()
-    for player_num, player_name in enumerate(players):
-        print(f"\n{player_num}, it's your turn!")
-        chosen_briefcase = int(input("Which briefcase do you choose? "))
-        briefcases.remove(chosen_briefcase)
-        winning_amount = winning_amounts.pop(random.randrange(len(winning_amounts)))
-        print(f"You have won ${winning_amount}!")
-        if chosen_briefcase == chosen_cases[player_num]:
-            print(f"\n{player_name}, you have won ${winning_amount} and your orginial briefcase is worth ${chosen_briefcase}!")
-            return
-    if round_num == num_rounds - 1:
-        print("\n\nFinal Round!")
-        offer_amount = offer()
+    
+    # Randomly choose a briefcase for each player
+    for player_name in players:
+        chosen_case = random.choice(briefcases)
+        chosen_cases.append(chosen_case)
+        briefcases.remove(chosen_case)
+        print(f"{player_name}, your chosen briefcase is: {chosen_case}")
+    
+    # Play the rounds
+    num_rounds = 6
+    for round_num in range(num_rounds):
+        print(f"\n\nRound {round_num+1}")
+        print_board()
         for player_num, player_name in enumerate(players):
             print(f"\n{player_name}, it's your turn!")
-            print_board()
-            deal_or_no_deal = input(f"You have been offered ${offer_amount}. Deal or no Deal? ")
-            if deal_or_no_deal.lower() == "deal":
-                print(f"\nCongratulations, {player_name}! You have won ${offer_amount}!")
+            chosen_briefcase = int(input("Which briefcase do you choose? "))
+            briefcases.remove(chosen_briefcase)
+            prize_amount = prize_amounts.pop(random.randrange(len(prize_amounts)))
+            print(f"You have won ${prize_amount}!")
+            if chosen_briefcase == chosen_cases[player_num]:
+                print(f"\n{player_name}, you have won ${prize_amount} and your original briefcase is worth ${chosen_briefcase}!")
                 return
-            elif deal_or_no_deal.lower() == "no deal":
-                continue
+        if round_num == num_rounds - 1:
+            print("\n\nFinal Round!")
+            offer_amount = offer()
+            for player_num, player_name in enumerate(players):
+                print(f"\n{player_name}, it's your turn!")
+                print_board()
+                deal_or_no_deal = input(f"You have been offered ${offer_amount}. Deal or no deal? ")
+                if deal_or_no_deal.lower() == "deal":
+                    print(f"\nCongratulations, {player_name}! You have won ${offer_amount}!")
+                    return
+                elif deal_or_no_deal.lower() == "no deal":
+                    continue
+                
+# Start the game
+play_game()
 
-play_game
         
         
               
