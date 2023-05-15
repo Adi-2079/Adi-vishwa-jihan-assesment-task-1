a32e55f4f12dd56e11ebbc9e8721d66c24d108b6
jihan-11 <127156455+jihan-11@users.noreply.github.com>
2023-05-16 09:50:20 +1000

    will now add backgound music on my laptop

--- Backgroundmusic.py
+++ Backgroundmusic.py
@@ -3,6 +3,6 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
 from pygame import mixer 
 
 mixer.init()
-audio_file = os.path.dirname(__file__) + "\\background.mp3"
+audio_file = os.path.dirname(__file__) + "\\background.wav"
 mixer.music.load(audio_file)
 mixer.music.play(loops=-1)
