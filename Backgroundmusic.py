5dcc114508aa75f8fb4a185e8ad5751cc8c599d4
jihan-11 <127156455+jihan-11@users.noreply.github.com>
2023-05-19 12:03:30 +1000

    login fixed, added back the code for the background music to play during the game

---
+++ Backgroundmusic.py
@@ -0,0 +1,9 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+from pygame import mixer
+
+
+mixer.init()
+audio_file = os.path.dirname(__file__) + "\\background.wav"
+mixer.music.load(audio_file)
+mixer.music.play(loops=-1)
