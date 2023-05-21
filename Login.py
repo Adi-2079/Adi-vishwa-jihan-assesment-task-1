ad95b9643c027343b2c6b3241980918d7d3e2a0a
Adi-2079 <aditya.mukhija@education.nsw.gov.au>
2023-05-22 04:03:58 +1000

    finished of code
    didi work

--- Login.py
+++ Login.py
@@ -6,10 +6,18 @@ def login():
     # Check if the username and password are valid
     with open("users.txt", "r") as f:
         for line in f:
-            user, passw = line.split()
-            if username == user and password == passw:
-                print("Login successful!")
-                return True
+            line = line.strip()  # Remove leading/trailing whitespaces
+            if not line:
+                continue  # Skip empty lines
+
+            try:
+                user, passw = line.split()
+                if username == user and password == passw:
+                    print("Login successful!")
+                    return True
+            except ValueError:
+                print("Invalid user data format in the file.")
+                return False
 
     # Display an error message
     print("Invalid username or password")
@@ -17,24 +25,33 @@ def login():
 
 
 def register():
-    
+    # Get the username and password from the user
     username = input("Enter your username: ")
     password = input("Enter your password: ")
 
     # Check if the username already exists
     with open("users.txt", "r") as f:
         for line in f:
-            user, passw = line.split()
-            if username == user:
-                print("Username already taken")
+            line = line.strip()  # Remove leading/trailing whitespaces
+            if not line:
+                continue  # Skip empty lines
+
+            try:
+                user, passw = line.split()
+                if username == user:
+                    print("Username already taken")
+                    return False
+            except ValueError:
+                print("Invalid user data format in the file.")
                 return False
 
-   
+    # Write the username and password to the text file
     with open("users.txt", "a") as f:
         f.write(f"{username} {password}\n")
 
     print("Registration successful!")
     return True
+
 
 
 def main():
@@ -64,4 +81,3 @@ def main():
 
 # Call the main function to start the login system
 main()
-
